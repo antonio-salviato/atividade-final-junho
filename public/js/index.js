0a03b90a213091ba8d73914c13b8cce6c51319ed
Antonio Salviato <aabsalviato@gmail.com>
2022-06-13 06:54:59 -0300

    initial comit

---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const loginUser = document.getElementById("loginUser");
+const passwordUser = document.getElementById("passwordUser");
+const submitbtn = document.getElementById("submitbtn");
+const formularioLogin = document.querySelector("#formularioLogin");
+const emailUser = loginUser.value;
+const senha = passwordUser.value;
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const loginUser = document.getElementById("loginUser") as HTMLInputElement;
+const passwordUser = document.getElementById("passwordUser") as HTMLInputElement;
+const submitbtn = document.getElementById("submitbtn") as HTMLButtonElement;
+const formularioLogin = document.querySelector("#formularioLogin")
+
+const emailUser = loginUser.value;
+const senha = passwordUser.value;
+
